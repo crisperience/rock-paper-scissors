7a9f710b56a9a6bab1a7640596426ae3f52de19a
CRISP <crisp@crisperience.com>
2024-09-09 14:46:53 +0200

    Added humanScore and computerScore variables

--- rock-papper-scissors.js
+++ rock-papper-scissors.js
@@ -1,8 +1,24 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
     const computerChoice = Math.floor(Math.random() * choices.length);
     return choices[computerChoice];
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+    let choice = prompt("Choose Rock, Paper, or Scissors:");
+    choice = choice.toLowerCase();
+    if (choice === "rock" || choice === "paper" || choice === "scissors") {
+        return choice;
+    } else {
+        console.log("Invalid choice! Please choose Rock, Paper, or Scissors.")
+        return getHumanChoice();
+    }
+}
+
+
+
+
 
